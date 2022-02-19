9af00ae4517efe3f08b2af7ff735bdaf98b3f5f2
drs-t0m0 <drs-tomo@hotmail.co.jp>
2022-02-19 20:52:05 +0900

    Palindromes

---
+++ palindrome/test.ts
@@ -0,0 +1,30 @@
+import {palindrome} from "./index.ts";
+import {assertEquals} from "https://deno.land/std@0.126.0/testing/asserts.ts";
+
+Deno.test("'aba' is a palindrome", () => {
+  assertEquals(palindrome("aba"), true);
+});
+
+Deno.test("' aba' is not a palindrome", () => {
+  assertEquals(palindrome(" aba"), false);
+});
+
+Deno.test("'aba ' is not a palindrome", () => {
+  assertEquals(palindrome("aba "), false);
+});
+
+Deno.test("'greetings' is not a palindrome", () => {
+  assertEquals(palindrome("greetings"), false);
+});
+
+Deno.test("'1000000001' is a palindrome", () => {
+  assertEquals(palindrome("1000000001"), true);
+});
+
+Deno.test("'Fish hsif' is not a palindrome", () => {
+  assertEquals(palindrome("Fish hsif"), false);
+});
+
+Deno.test("'pennep' is a palindrome", () => {
+  assertEquals(palindrome("pennep"), true);
+});
