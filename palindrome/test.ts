66f4a8ff3b6bca319d7f3da73b51f45aebdacff7
drs-t0m0 <drs-tomo@hotmail.co.jp>
2022-02-19 20:53:40 +0900

    bug fix

--- palindrome/test.ts
+++ palindrome/test.ts
@@ -1,5 +1,5 @@
-import {palindrome} from "./index.ts";
-import {assertEquals} from "https://deno.land/std@0.126.0/testing/asserts.ts";
+import { palindrome } from "./index.ts";
+import { assertEquals } from "https://deno.land/std@0.126.0/testing/asserts.ts";
 
 Deno.test("'aba' is a palindrome", () => {
   assertEquals(palindrome("aba"), true);
